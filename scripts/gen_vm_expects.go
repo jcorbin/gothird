1906dade49754c655ab12e534fb70cd9da8da8bd
Joshua T Corbin <jcorbin@wunjo.org>
2020-04-26 09:52:11 -0700

    generate withVM* conveniences too

--- scripts/gen_vm_expects.go
+++ scripts/gen_vm_expects.go
@@ -35,7 +35,7 @@ func parseFlags() {
 		name := args[0]
 		f, err := os.Open(name)
 		if err != nil {
-			log.Fatalln("failed to open %v: %v", name, err)
+			log.Fatalf("failed to open %v: %v", name, err)
 		}
 		args = args[1:]
 		in = f
@@ -45,7 +45,7 @@ func parseFlags() {
 		name := args[0]
 		f, err := os.Create(name)
 		if err != nil {
-			log.Fatalln("failed to create %v: %v", name, err)
+			log.Fatalf("failed to create %v: %v", name, err)
 		}
 		args = args[1:]
 		out = f
@@ -64,7 +64,7 @@ func main() {
 	ready := make(chan struct{})
 
 	eg.Go(func() error {
-		gofmt := exec.CommandContext(ctx, "gofmt")
+		gofmt := exec.CommandContext(ctx, "goimports")
 		fmtPipe, err := gofmt.StdinPipe()
 		if err != nil {
 			return err
@@ -106,7 +106,7 @@ func main() {
 	}
 }
 
-var expectMethod = regexp.MustCompile(`func \(vmt vmTestCase\) expect(.+?)\((.+?)\) vmTestCase`)
+var expectMethod = regexp.MustCompile(`func \(vmt vmTestCase\) (expect|with)(.+?)\((.+?)\) vmTestCase`)
 
 func run(ctx context.Context) error {
 	var buf bytes.Buffer
@@ -129,17 +129,25 @@ func run(ctx context.Context) error {
 	sc := bufio.NewScanner(in)
 	for sc.Scan() {
 		if match := expectMethod.FindSubmatch(sc.Bytes()); len(match) > 0 {
-			buf.WriteString("func expectVM")
-			buf.Write(match[1])
+			var (
+				baseName = match[1]
+				whatName = match[2]
+				args     = match[3]
+			)
+			buf.WriteString("func ")
+			buf.Write(baseName)
+			buf.WriteString("VM")
+			buf.Write(whatName)
 			buf.WriteString("(")
-			buf.Write(match[2])
+			buf.Write(args)
 			buf.WriteString(") func(vmTestCase) vmTestCase {\n")
 			buf.WriteString("  return func(vmt vmTestCase) vmTestCase {\n")
-			buf.WriteString("    return vmt.expect")
-			buf.Write(match[1])
+			buf.WriteString("    return vmt.")
+			buf.Write(baseName)
+			buf.Write(whatName)
 			buf.WriteString("(")
 
-			for i, part := range bytes.Split(match[2], []byte(",")) {
+			for i, part := range bytes.Split(args, []byte(",")) {
 				if i > 0 {
 					buf.WriteString(", ")
 				}
--- vm_expects_test.go
+++ vm_expects_test.go
@@ -1,8 +1,115 @@
 package main
 
+import (
+	"io"
+	"time"
+)
+
 // @generated from vm_test.go
 
 //go:generate go run scripts/gen_vm_expects.go -- vm_test.go vm_expects_test.go
+
+func withVMOptions(opts ...VMOption) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withOptions(opts...)
+	}
+}
+
+func withVMProg(prog uint) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withProg(prog)
+	}
+}
+
+func withVMLast(last uint) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withLast(last)
+	}
+}
+
+func withVMStack(values ...int) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withStack(values...)
+	}
+}
+
+func withVMStrings(idStringPairs ...interface{}) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withStrings(idStringPairs...)
+	}
+}
+
+func withVMString(id uint, s string) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withString(id, s)
+	}
+}
+
+func withVMMemAt(addr uint, values ...int) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withMemAt(addr, values...)
+	}
+}
+
+func withVMH(val int) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withH(val)
+	}
+}
+
+func withVMR(val int) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withR(val)
+	}
+}
+
+func withVMPageSize(pageSize uint) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withPageSize(pageSize)
+	}
+}
+
+func withVMRetBase(addr uint, values ...int) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withRetBase(addr, values...)
+	}
+}
+
+func withVMMemBase(addr uint, values ...int) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withMemBase(addr, values...)
+	}
+}
+
+func withVMMemLimit(limit uint) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withMemLimit(limit)
+	}
+}
+
+func withVMInput(input string) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withInput(input)
+	}
+}
+
+func withVMNamedInput(name string, input string) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withNamedInput(name, input)
+	}
+}
+
+func withVMInputWriter(w io.WriterTo) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withInputWriter(w)
+	}
+}
+
+func withVMTimeout(timeout time.Duration) func(vmTestCase) vmTestCase {
+	return func(vmt vmTestCase) vmTestCase {
+		return vmt.withTimeout(timeout)
+	}
+}
 
 func expectVMError(err error) func(vmTestCase) vmTestCase {
 	return func(vmt vmTestCase) vmTestCase {
