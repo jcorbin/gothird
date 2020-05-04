8fc74386e7a7ab2ebb64307d3a76d66e0d508eee
Joshua T Corbin <jcorbin@wunjo.org>
2020-05-03 21:50:07 -0700

    generate third kernel from test

--- third.go
+++ third.go
@@ -5,58 +5,117 @@ import (
 	"strings"
 )
 
-const thirdSource = `
+//go:generate go test -generate-third .
 
+const _thirdSource = `
 exit : immediate _read @ ! - * / <0 echo key pick
 
 : r 1 exit
 
-: ] r @ 1 - r ! _read ]
+: ]
+  r @
+  1 -
+  r !
+  _read ]
 
-: main immediate r @ 7 ! ]
-main
+: main immediate ] main
+
+: rb 10 exit
 
 : _x  3 @ exit
 : _x! 3 ! exit
-: _y  4 @ exit
-: _y! 4 ! exit
 
-: swap _x! _y! _x _y exit
-
-: + 0 swap - - exit
+: + _x! 0 _x - - exit
 
 : dup _x! _x _x exit
 
-: inc dup @ 1 + swap ! exit
+: '
+  r @ @
+  dup
+  1 +
+  r @ !
+  @
+  exit
+
+: exec
+  rb @ !
+  rb @ r !
+  exit
+
+: _read] _read ]
+: reboot immediate
+  ' _read]
+  exec
+reboot
+
+: _y  4 @ exit
+: _y! 4 ! exit
+
+: swap
+  _x! _y! _x _y
+  exit
+
+: inc
+  dup @
+  1 +
+  swap !
+  exit
 
 : h 0 exit
 
-: , h @ ! h inc exit
-
-: ' r @ @ dup 1 + r @ ! @ exit
+: ,
+  h @
+  !
+  h inc
+  exit
 
 : ; immediate ' exit , exit
 
 : drop 0 * + ;
 
-: dec dup @ 1 - swap ! ;
+: dec
+  dup @
+  1 -
+  swap !
+  ;
 
-: tor r @ @ swap r @ ! r @ 1 + r ! r @ ! ;
-: fromr r @ @ r @ 1 - r ! r @ @ swap r @ ! ;
+: tor
+  r @ @
+  swap
+  r @ !
+  r @ 1 + r !
+  r @ !
+  ;
 
-: tail fromr fromr drop tor ;
+: fromr
+  r @ @
+  r @ 1 - r !
+  r @ @
+  swap
+  r @ !
+  ;
 
 : minus 0 swap - ;
 : bnot 1 swap - ;
 : < - <0 ;
-
-: logical dup 0 < swap minus 0 < + ;
+: logical
+	dup
+	0 <
+	swap minus
+	0 <
+	+
+	;
 : not logical bnot ;
 : = - not ;
 
-: branch r @ @ @ r @ @ + r @ ! ;
+: branch
+  r @ @
+  @
+  r @ @
+  +
+  r @ !
+  ;
 : computebranch 1 - * 1 + ;
-
 : notbranch
   not
   r @ @ @
@@ -67,21 +126,27 @@ main
 
 : here h @ ;
 
-: if immediate ' notbranch , here 0 , ;
-: then immediate dup here swap - swap ! ;
+: if immediate
+  ' notbranch ,
+  here
+  0 ,
+  ;
 
-: find-) key ')' = not if tail find-) then ;
+: then immediate
+  dup
+  here swap -
+  swap !
+  ;
 
+: tail fromr fromr drop tor ;
+
+: find-)
+  key
+  ')' =
+  not if
+  tail find-)
+  then ;
 : ( immediate find-) ;
-
-( we should be able to do FORTH-style comments now )
-
-( this works as follows: ( is an immediate word, so it gets
-  control during compilation.  Then it simply reads in characters
-  until it sees a close parenthesis.  once it does, it exits.
-  if not, it pops off the return stack--manual tail recursion. )
-
-( now that we've got comments, we can comment the rest of the code! )
 
 : else immediate
   ' branch ,            ( compile a definite branch )
@@ -92,38 +157,17 @@ main
   swap !                ( put the address on top and store it )
 ;
 
-: over _x! _y! _y _x _y ;
-
-: add
-  _x!                   ( save the pointer in a temp variable )
-  _x @                  ( get the value pointed to )
-  +                     ( add the increment from on top of the stack )
-  _x !                  ( and save it )
-;
-
-: allot h add ;
-
-: maybebranch
-  logical               ( force the TOS to be 0 or 1 )
-  r @ @ @               ( load the branch offset )
-  computebranch         ( calculate the condition offset [either TOS or 1])
-  r @ @ +               ( add it to the return address )
-  r @ !                 ( store it to our return address and return )
-;
-
 : mod _x! _y!           ( get x then y off of stack )
   _y _y _x / _x *       ( y - y / x * x )
   -
 ;
-
-: nl '\n' echo exit
 
 : printnum
   dup
   10 mod '0' +
   swap 10 / dup
   if
-    printnum 0
+	printnum 0
   then
   drop echo
 ;
@@ -131,52 +175,20 @@ main
 : .
   dup 0 <
   if
-    '-' echo minus
+	'-' echo minus
   then
   printnum
   <sp> echo
 ;
 
-: debugprint dup . nl ;
+: nl <nl> echo ;
 
-( the following routine takes a pointer to a string, and prints it,
-  except for the trailing quote.  returns a pointer to the next word
-  after the trailing quote )
+: > swap < ;
+: <= 1 + < ;
+: >= swap <= ;
 
-: _print
-  dup 1 +
-  swap @
-  dup '"' =
-  if
-    drop exit
-  then
-  echo
-  tail _print
-;
-
-: print _print ;
-
-( print the next thing from the instruction stream )
-: immprint
-  r @ @
-  print
-  r @ !
-;
-
-: find-"
-  key dup ,
-  '"' =
-  if
-    exit
-  then
-  tail find-"
-;
-
-: " immediate
-  key drop
-  ' immprint ,
-  find-"
-;
+: i r @ 1 - @ ;
+: j r @ 3 - @ ;
 
 : do immediate
   ' swap ,              ( compile 'swap' to swap the limit and start )
@@ -185,13 +197,6 @@ main
   here                  ( save this address so we can branch back to it )
 ;
 
-: i r @ 1 - @ ;
-: j r @ 3 - @ ;
-
-: > swap < ;
-: <= 1 + < ;
-: >= swap <= ;
-
 : inci
   r @ 1 -               ( get the pointer to i )
   inc                   ( add one to it )
@@ -199,7 +204,10 @@ main
   r @ 2 - @             ( find the limit value )
   <
   if
-    r @ @ @ r @ @ + r @ ! exit          ( branch )
+	r @ @ @
+	r @ @ +
+	r @ !
+	exit          ( branch )
   then
   fromr 1 +
   fromr drop
@@ -213,36 +221,39 @@ main
   fromr drop            ( pop off our return address )
   fromr drop            ( pop off i )
   fromr drop            ( pop off the limit of i )
-  ;                     ( and return to the caller's caller routine )
+;                       ( and return to the caller's caller routine )
 
-: :: ;                  ( :: is going to be a word that does ':' at runtime )
-
+( :: is going to be a word that does ':' at runtime )
+: :: ; ;
 : fix-:: immediate 1 ' :: ! ; ( vmCodeDefine = 1 )
 fix-::
 
 ( Override old definition of ':' with a new one that invokes ] )
 : : immediate :: r dec ] ;
 
+: _z  5 @ exit
+: _z! 5 ! exit
+
 : execute
   dup not if            ( execute an exit on behalf of caller )
-	fromr drop          ( pop up to the caller's return )
-	8 !                 ( hacked unary drop of the exit code )
-	;                   ( back to the caller's caller )
+    fromr drop          ( pop up to the caller's return )
+    8 !                 ( hacked unary drop of the exit code )
+    ;                   ( back to the caller's caller )
   then
   8 !                   ( write code into temporary region )
   ' exit 9 !            ( with a following exit )
   8 tor ;               ( jump into temporary region )
 
 : command
-  here 5 !              ( store dict pointer in temp variable )
+  here _z!              ( store dict pointer in temp variable )
   _read                 ( compile a word )
                         ( if we get control back: )
-  here 5 @
+  here _z
   = if
     tail command        ( we didn't compile anything )
   then
   here 1 - h !          ( decrement the dictionary pointer )
-  here 5 @              ( get the original value )
+  here _z               ( get the original value )
   = if
     here @              ( get the word that was compiled )
     execute             ( and run it )
@@ -254,59 +265,6 @@ fix-::
 ;
 
 : [ immediate command ;
-
-: make-immediate        ( make a word just compiled immediate )
-  here 1 -              ( back up a word in the dictionary )
-  dup dup               ( save the pointer to here )
-  h !                   ( store as the current dictionary pointer )
-  @                     ( get the run-time code pointer )
-  swap                  ( get the dict pointer again )
-  1 -                   ( point to the compile-time code pointer )
-  !                     ( write run-time code pointer on compile-time pointer )
-;
-
-: <build immediate
-  make-immediate        ( make the word compiled so far immediate )
-  ' :: ,                ( compile '::', so we read next word )
-  2 ,                   ( compile 'pushint' )
-  here 0 ,              ( write out a 0 but save address for does> )
-  ' , ,                 ( compile a push that address onto dictionary )
-;
-
-: does> immediate
-  ' command ,           ( jump back into command mode at runtime )
-  here swap !           ( backpatch the build> to point to here )
-  2 ,                   ( compile run-code primitive so we look like a word )
-  ' fromr ,             ( compile fromr, which leaves var address on stack )
-;
-
-: _dump                 ( dump out the definition of a word, sort of )
-  dup " (" . " , "
-  dup @                 ( save the pointer and get the contents )
-  dup ' exit
-  = if
-        " ;)" nl exit
-  then
-  . " ), "
-  1 +
-  tail _dump
-;
-
-: dump _dump ;
-
-: # . nl ;
-
-: var <build , does> ;
-: constant <build , does> @ ;
-: array <build allot does> + ;
-
-: _welcome " Welcome to THIRD.
-Ok.
-" ;
-
-[
-
-_welcome
 `
 
 type _thirdKernel struct{}
@@ -316,9 +274,9 @@ func (_thirdKernel) Name() string { return "third" }
 func (_thirdKernel) WriteTo(w io.Writer) (_ int64, err error) {
 	var n int
 	if sw, ok := w.(io.StringWriter); ok {
-		n, err = sw.WriteString(thirdSource)
+		n, err = sw.WriteString(_thirdSource)
 	} else {
-		n, err = w.Write([]byte(thirdSource))
+		n, err = w.Write([]byte(_thirdSource))
 	}
 	return int64(n), err
 }
@@ -327,7 +285,7 @@ func (_thirdKernel) Open() io.Reader {
 	return struct {
 		_thirdKernel
 		io.Reader
-	}{Reader: strings.NewReader(thirdSource)}
+	}{Reader: strings.NewReader(_thirdSource)}
 }
 
 var thirdKernel _thirdKernel
--- third_test.go
+++ third_test.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
 	"strings"
 	"testing"
+	"text/template"
 	"time"
 )
+
+var testThirdKernel = kernel{name: "third"}
 
 // Test_kernel tests a layered recreation of the THIRD kernel, with a test case
 // validating each each layer.
 func Test_kernel(t *testing.T) {
-	var k kernel
-
 	// Test that builtin word initialization works.
-	k.addSource("builtins", `
+	testThirdKernel.addSource("builtins", `
 		exit : immediate _read @ ! - * / <0 echo key pick
 	`, "",
 		expectVMWord(1024, "", vmCodeRun, vmCodeRead, 1027, vmCodeExit),
@@ -58,14 +67,14 @@ func Test_kernel(t *testing.T) {
 		)))
 
 	// Build a main loop that won't exhaust the return stack.
-	k.addSource("main", `
+	testThirdKernel.addSource("main", `
 		: r 1 exit
 
 		: ]
-			r @
-			r -
-			r !
-			_read ]
+		  r @
+		  1 -
+		  r !
+		  _read ]
 
 		: main immediate ] main
 
@@ -79,17 +88,17 @@ func Test_kernel(t *testing.T) {
 			/* @1101 */ vmCodeCompile,
 			/* @1102 */ vmCodeRun,
 			/* @1103 */ 1096, vmCodeGet,
-			/* @1105 */ 1096, vmCodeSub,
-			/* @1107 */ 1096, vmCodeSet,
-			/* @1109 */ vmCodeRead,
-			/* @1110 */ 1103,
+			/* @1105 */ 15, 1, vmCodeSub,
+			/* @1108 */ 1096, vmCodeSet,
+			/* @1110 */ vmCodeRead,
+			/* @1111 */ 1103,
 		),
-		expectVMWord(1111, "main", vmCodeRun, 1103),
-		expectVMWord(1115, "rb", vmCodeCompile, vmCodeRun, vmCodePushint, 10, vmCodeExit),
+		expectVMWord(1112, "main", vmCodeRun, 1103),
+		expectVMWord(1116, "rb", vmCodeCompile, vmCodeRun, vmCodePushint, 10, vmCodeExit),
 		expectVMStack(256))
 
 	// Synthesize addition and test it.
-	k.addSource("add", `
+	testThirdKernel.addSource("add", `
 		: _x  3 @ exit
 		: _x! 3 ! exit
 
@@ -98,35 +107,35 @@ func Test_kernel(t *testing.T) {
 		: test immediate
 			3 5 7 + +
 	`,
-		expectVMWord(1122, "_x",
-			/* 1124 */ vmCodeCompile, vmCodeRun,
-			/* 1126 */ vmCodePushint, 3, vmCodeGet,
-			/* 1129 */ vmCodeExit),
-		expectVMWord(1130, "_x!",
-			/* 1132 */ vmCodeCompile, vmCodeRun,
-			/* 1134 */ vmCodePushint, 3, vmCodeSet,
-			/* 1137 */ vmCodeExit),
-		expectVMWord(1138, "+",
-			/* 1140 */ vmCodeCompile, vmCodeRun,
-			/* 1142 */ 1134, // _x!
-			/* 1145 */ vmCodePushint, 0,
-			/* 1146 */ 1126, // _x
-			/* 1147 */ vmCodeSub, vmCodeSub,
-			/* 1149 */ vmCodeExit),
+		expectVMWord(1123, "_x",
+			/* 1125 */ vmCodeCompile, vmCodeRun,
+			/* 1127 */ vmCodePushint, 3, vmCodeGet,
+			/* 1130 */ vmCodeExit),
+		expectVMWord(1131, "_x!",
+			/* 1133 */ vmCodeCompile, vmCodeRun,
+			/* 1135 */ vmCodePushint, 3, vmCodeSet,
+			/* 1138 */ vmCodeExit),
+		expectVMWord(1139, "+",
+			/* 1141 */ vmCodeCompile, vmCodeRun,
+			/* 1143 */ 1135, // _x!
+			/* 1146 */ vmCodePushint, 0,
+			/* 1147 */ 1127, // _x
+			/* 1148 */ vmCodeSub, vmCodeSub,
+			/* 1150 */ vmCodeExit),
 		expectVMStack(3+5+7))
 
 	// ' is a standard FORTH word.
 	// It should push the address of the word that follows it onto the stack.
-	k.addSource("quote", `
+	testThirdKernel.addSource("quote", `
 		: dup _x! _x _x exit
 
 		: '
-			r @ @
-			dup
-			1 +
-			r @ !
-			@
-			exit
+		  r @ @
+		  dup
+		  1 +
+		  r @ !
+		  @
+		  exit
 	`, `
 		: test immediate
 			' ]
@@ -139,34 +148,34 @@ func Test_kernel(t *testing.T) {
 	))
 
 	// Reset the return stack, by way of an "exec" word.
-	k.addSource("reboot", `
+	testThirdKernel.addSource("reboot", `
 		: exec
-			rb @ !
-			rb @ r !
-			exit
+		  rb @ !
+		  rb @ r !
+		  exit
 
 		: _read] _read ]
 		: reboot immediate
-			' _read]
-			exec
+		  ' _read]
+		  exec
 		reboot
 	`, `
 		: test immediate
 			42
 			1024 1024 * @
 	`,
-		expectVMRStack(1110),
+		expectVMRStack(1111),
 		expectVMStack(42),
 		expectVMError(memLimitError{1024 * 1024, "get"}))
 
 	// swap two values on the top of the stack.
-	k.addSource("swap", `
+	testThirdKernel.addSource("swap", `
 		: _y  4 @ exit
 		: _y! 4 ! exit
 
 		: swap
-			_x! _y! _x _y
-			exit
+		  _x! _y! _x _y
+		  exit
 	`, `
 		: test immediate
 			swap
@@ -176,7 +185,7 @@ func Test_kernel(t *testing.T) {
 		expectVMStack(99, 44, 100))
 
 	// inc is a pointer incrementing word.
-	k.addSource("inc", `
+	testThirdKernel.addSource("inc", `
 		: inc
 		  dup @
 		  1 +
@@ -191,25 +200,25 @@ func Test_kernel(t *testing.T) {
 
 	// , is a standard FORTH word.
 	// It should write the top of stack into the dictionary, and advance the pointer.
-	k.addSource("compile", `
+	testThirdKernel.addSource("compile", `
 		: h 0 exit
 
 		: ,
-			h @
-			!
-			h inc
-			exit
+		  h @
+		  !
+		  h inc
+		  exit
 	`, `
 		: test immediate
 			42 ,
 			exit
 	`,
-		expectVMH(1284),
-		expectVMMemAt(1283, 42))
+		expectVMH(1285),
+		expectVMMemAt(1284, 42))
 
 	// ; should be an immediate word that pushes the address of exit onto the
 	// stack, then writes it out.
-	k.addSource(";", `
+	testThirdKernel.addSource(";", `
 		: ; immediate ' exit , exit
 
 		: drop 0 * + ;
@@ -224,7 +233,7 @@ func Test_kernel(t *testing.T) {
 		expectVMStack(9, 108))
 
 	// dec is a pointer decrementing word.
-	k.addSource("dec", `
+	testThirdKernel.addSource("dec", `
 		: dec
 		  dup @
 		  1 -
@@ -239,7 +248,7 @@ func Test_kernel(t *testing.T) {
 
 	// tor transfers a value from the data stack to the return stack.
 	// It's life is complicated, due to needing to work around its own return.
-	k.addSource("tor", `
+	testThirdKernel.addSource("tor", `
 		: tor
 		  r @ @
 		  swap
@@ -263,7 +272,7 @@ func Test_kernel(t *testing.T) {
 
 	// fromr transfers a values from the return tack to the data stack.
 	// It's life is complicated, due to needing to work around its own return.
-	k.addSource("fromr", `
+	testThirdKernel.addSource("fromr", `
 		: fromr
 		  r @ @
 		  r @ 1 - r !
@@ -285,11 +294,10 @@ func Test_kernel(t *testing.T) {
 	`,
 		expectVMOutput(`ah`))
 
-	k.addSource("bool", `
+	testThirdKernel.addSource("bool", `
 		: minus 0 swap - ;
 		: bnot 1 swap - ;
 		: < - <0 ;
-		: > minus < ;
 		: logical
 			dup
 			0 <
@@ -307,36 +315,36 @@ func Test_kernel(t *testing.T) {
 			;
 	`, expectVMStack(1, 0, 1))
 
-	k.addSource("branch", `
+	testThirdKernel.addSource("branch", `
 		: branch
-			r @ @
-			@
-			r @ @
-			+
-			r @ !
-			;
+		  r @ @
+		  @
+		  r @ @
+		  +
+		  r @ !
+		  ;
 		: computebranch 1 - * 1 + ;
 		: notbranch
-			not
-			r @ @ @
-			computebranch
-			r @ @ +
-			r @ !
-			;
+		  not
+		  r @ @ @
+		  computebranch
+		  r @ @ +
+		  r @ !
+		  ;
 
 		: here h @ ;
 
 		: if immediate
-			' notbranch ,
-			here
-			0 ,
-			;
+		  ' notbranch ,
+		  here
+		  0 ,
+		  ;
 
 		: then immediate
-			dup
-			here swap -
-			swap !
-			;
+		  dup
+		  here swap -
+		  swap !
+		  ;
 	`, `
 		: test immediate
 			'h' echo
@@ -346,7 +354,7 @@ func Test_kernel(t *testing.T) {
 			'a' echo
 	`, expectVMOutput(`ha`))
 
-	k.addSource("tail", `
+	testThirdKernel.addSource("tail", `
 		: tail fromr fromr drop tor ;
 	`, `
 		: printn
@@ -382,13 +390,13 @@ func Test_kernel(t *testing.T) {
 			;
 	`, expectVMOutput(`"hello"`))
 
-	k.addSource("comments", `
+	testThirdKernel.addSource("comments", `
 		: find-)
-			key
-			')' =
-			not if
-			tail find-)
-			then ;
+		  key
+		  ')' =
+		  not if
+		  tail find-)
+		  then ;
 		: ( immediate find-) ;
 	`, `
 		: test immediate
@@ -398,7 +406,7 @@ func Test_kernel(t *testing.T) {
 			;
 	`, expectVMOutput(`//`))
 
-	k.addSource("else", `
+	testThirdKernel.addSource("else", `
 		: else immediate
 		  ' branch ,            ( compile a definite branch )
 		  here                  ( push the backpatching address )
@@ -421,7 +429,7 @@ func Test_kernel(t *testing.T) {
 			;
 	`, expectVMOutput(`nyyy`))
 
-	k.addSource("printnum", `
+	testThirdKernel.addSource("printnum", `
 		: mod _x! _y!           ( get x then y off of stack )
 		  _y _y _x / _x *       ( y - y / x * x )
 		  -
@@ -432,11 +440,9 @@ func Test_kernel(t *testing.T) {
 		  10 mod '0' +
 		  swap 10 / dup
 		  if
-			printnum
-		  else
-			drop
+			printnum 0
 		  then
-		  echo
+		  drop echo
 		;
 
 		: .
@@ -448,19 +454,17 @@ func Test_kernel(t *testing.T) {
 		  <sp> echo
 		;
 
-		: cr
-			<nl> echo
-			;
+		: nl <nl> echo ;
 	`, `
 		: test immediate
 			99 .
 			44 .
 			100 .
-			cr
+			nl
 			;
 	`, expectVMOutput("99 44 100 \n"))
 
-	k.addSource("more bools", `
+	testThirdKernel.addSource("more bools", `
 		: > swap < ;
 		: <= 1 + < ;
 		: >= swap <= ;
@@ -484,7 +488,7 @@ func Test_kernel(t *testing.T) {
 		1, 1, 0,
 	))
 
-	k.addSource("loop", `
+	testThirdKernel.addSource("loop", `
 		: i r @ 1 - @ ;
 		: j r @ 3 - @ ;
 
@@ -522,11 +526,11 @@ func Test_kernel(t *testing.T) {
 		;                       ( and return to the caller's caller routine )
 	`, `
 		: test immediate
-		  11 1 do i . loop cr
+		  11 1 do i . loop nl
 		  ;
 	`, expectVMOutput("1 2 3 4 5 6 7 8 9 10 \n"))
 
-	k.addSource("runtime define", `
+	testThirdKernel.addSource("runtime define", `
 		( :: is going to be a word that does ':' at runtime )
 		: :: ; ;
 		: fix-:: immediate 1 ' :: ! ; ( vmCodeDefine = 1 )
@@ -543,14 +547,14 @@ func Test_kernel(t *testing.T) {
 		  ;
 	`, expectVMRStack(), expectVMOutput(`abab`))
 
-	k.addSource("command mode", `
+	testThirdKernel.addSource("command mode", `
 		: _z  5 @ exit
 		: _z! 5 ! exit
 
 		: execute
 		  dup not if            ( execute an exit on behalf of caller )
 		    fromr drop          ( pop up to the caller's return )
-			8 !                 ( hacked unary drop of the exit code )
+		    8 !                 ( hacked unary drop of the exit code )
 		    ;                   ( back to the caller's caller )
 		  then
 		  8 !                   ( write code into temporary region )
@@ -563,16 +567,16 @@ func Test_kernel(t *testing.T) {
 		                        ( if we get control back: )
 		  here _z
 		  = if
-			tail command        ( we didn't compile anything )
+		    tail command        ( we didn't compile anything )
 		  then
 		  here 1 - h !          ( decrement the dictionary pointer )
 		  here _z               ( get the original value )
 		  = if
-			here @              ( get the word that was compiled )
-			execute             ( and run it )
+		    here @              ( get the word that was compiled )
+		    execute             ( and run it )
 		  else
-			here @              ( else it was an integer constant, so push it )
-			here 1 - h !        ( and decrement the dictionary pointer again )
+		    here @              ( else it was an integer constant, so push it )
+		    here 1 - h !        ( and decrement the dictionary pointer again )
 		  then
 		  tail command
 		;
@@ -580,13 +584,16 @@ func Test_kernel(t *testing.T) {
 		: [ immediate command ;
 	`, `tron
 
-		[ 42 . cr ;
+		[ 42 . nl ;
 
-		108 . cr
+		108 . nl
 	`, expectVMStack(), expectVMOutput("42 \n"))
 
-	k.tests.run(t)
+	testThirdKernel.tests.run(t)
 }
+
+var genThirdFlag = flag.Bool("generate-third", false,
+	"generate the third.go kernel source from the tested sources in third_test.go")
 
 // Test_third tests a, minimally modified copy of, the original third kernel code.
 func Test_Third(t *testing.T) {
@@ -599,7 +606,130 @@ func Test_Third(t *testing.T) {
 		run(t)
 }
 
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	exitCode := m.Run()
+
+	if *genThirdFlag && exitCode == 0 {
+		if err := generateFile("third.go", func(w io.Writer) error {
+			return withGoimports(w, func(w io.Writer) error {
+				io.WriteString(w, "package main\n\n")
+				fmt.Fprintf(w, "//go:generate go test -generate-third .\n\n")
+				return kernelTmpl.Execute(w, testThirdKernel)
+			})
+		}); err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR generating tested third kernel: %+v\n", err)
+			exitCode = 1
+		}
+	}
+
+	os.Exit(exitCode)
+}
+
+var kernelTmpl = template.Must(template.New("").Parse(`
+const {{ .SourceName }} = {{ .QuotedSource }}
+
+type {{ .TypeName }} struct{}
+
+func ({{ .TypeName }}) Name() string { return "{{ .FileName }}" }
+
+func ({{ .TypeName }}) WriteTo(w io.Writer) (_ int64, err error) {
+	var n int
+	if sw, ok := w.(io.StringWriter); ok {
+		n, err = sw.WriteString({{ .SourceName }})
+	} else {
+		n, err = w.Write([]byte({{ .SourceName }}))
+	}
+	return int64(n), err
+}
+
+func ({{ .TypeName }}) Open() io.Reader {
+	return struct {
+		{{ .TypeName }}
+		io.Reader
+	}{Reader: strings.NewReader({{ .SourceName }})}
+}
+
+var {{ .VarName }} {{ .TypeName }}
+`))
+
+func (k kernel) FileName() string   { return k.name }
+func (k kernel) SourceName() string { return "_" + k.name + "Source" }
+func (k kernel) TypeName() string   { return "_" + k.name + "Kernel" }
+func (k kernel) VarName() string    { return k.name + "Kernel" }
+func (k kernel) QuotedSource() string {
+	const includeNameComments = false
+	var sb strings.Builder
+	var quote rune
+	for i, input := range k.inputs {
+		input = strings.TrimRight(input, " \t\n")
+
+		if includeNameComments {
+			if quote != 0 {
+				sb.WriteRune(quote)
+				sb.WriteRune('+')
+				sb.WriteRune('\n')
+				quote = 0
+			}
+			fmt.Fprintf(&sb, "/* %v */", k.names[i])
+		}
+
+		first := true
+		indent := ""
+		for _, line := range strings.Split(input, "\n") {
+			// detect indent from initial non-empty line
+			if first && len(line) > 0 {
+				rest := strings.TrimLeft(line, " \t")
+				if n := len(line) - len(rest); n > 0 {
+					indent = line[:n]
+				}
+				first = false
+			}
+
+			// trim any indent
+			if indent != "" {
+				line = strings.TrimPrefix(line, indent)
+			}
+
+			// determine needed quoting
+			needQuote := '`'
+			if !strconv.CanBackquote(line) {
+				line = strconv.Quote(line)
+				needQuote = []rune(line)[0]
+				line = line[1 : len(line)-1]
+			}
+
+			// transition quote state
+			if quote != needQuote {
+				if quote != 0 {
+					sb.WriteRune(quote)
+					sb.WriteRune('+')
+					sb.WriteRune('\n')
+				}
+				sb.WriteRune(needQuote)
+				quote = needQuote
+			}
+
+			// write line content
+			sb.WriteString(line)
+
+			// write line end
+			if quote == '`' {
+				sb.WriteRune('\n')
+			} else {
+				sb.WriteString(`\n`)
+			}
+		}
+	}
+	if quote != 0 {
+		sb.WriteRune(quote)
+	}
+	return sb.String()
+}
+
 type kernel struct {
+	name   string
 	names  []string
 	inputs []string
 	tests  vmTestCases
@@ -649,4 +779,59 @@ func (k *kernel) addSource(
 	k.names = append(k.names, name)
 	k.inputs = append(k.inputs, input)
 	k.tests = append(k.tests, vmt)
+}
+
+func withGoimports(out io.Writer, under func(w io.Writer) error) (rerr error) {
+	goimp := exec.Command("goimports")
+
+	in, err := goimp.StdinPipe()
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	goimp.Stdout = out
+	goimp.Stderr = os.Stderr
+	if err := goimp.Start(); err != nil {
+		return err
+	}
+	defer func() {
+		if werr := goimp.Wait(); rerr == nil {
+			rerr = werr
+		}
+	}()
+
+	defer func() {
+		if cerr := in.Close(); rerr == nil {
+			rerr = cerr
+		}
+	}()
+
+	return under(in)
+}
+
+func generateFile(name string, under func(w io.Writer) error) (rerr error) {
+	f, err := ioutil.TempFile(filepath.Dir(name), name+".tmp*")
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if rerr == nil {
+			rerr = renameTempFile(name, f)
+		}
+		if rerr != nil {
+			os.Remove(f.Name())
+		}
+	}()
+	return under(f)
+}
+
+func renameTempFile(dest string, f *os.File) error {
+	if err := f.Sync(); err != nil {
+		return err
+	}
+	if err := f.Close(); err != nil {
+		return err
+	}
+	return os.Rename(f.Name(), dest)
 }
